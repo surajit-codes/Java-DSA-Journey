5c18b6fdc3642b505eb44af819f8132975f4a192
surajit-codes <isurajitmondal06@gmail.com>
2026-08-19 19:49:20 +0530

    Master Sheet: Solved Add 1 to LL in O(1) space (Platform Accepted logic)

---
+++ src/DSA_Master_Sheet/Topic_08_LinkedList/Add1ToALinkedListNumber.java
@@ -0,0 +1,44 @@
+package DSA_Master_Sheet.Topic_08_LinkedList;
+/*
+    * Problem Name: Add 1 to a LinkedList Number
+    * Difficulty: Medium , Level 4/5 (DSA_Master_Sheet)
+    * Platform: GFG (https://www.geeksforgeeks.org/problems/add-1-to-a-number-represented-as-linked-list/1)
+ */
+public class Add1ToALinkedListNumber {
+    public Node addOne(Node head) {
+        Node temp = head;
+        Node back = head;
+        Node prev = null;
+        Node cur = head;
+        Node fwd = null;
+        // reverse the list
+        while(cur != null){
+            fwd = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = fwd;
+        }
+        back = prev;
+        // last dig increment by one
+        back.val++;
+        // breaking 2 digs num
+        while(back.next != null){
+            if(back.val == 10){
+                back.val = 0;
+                back.next.val++;
+            }
+            back = back.next;
+        }
+        cur = prev;
+        fwd = null;
+        back = null;
+        //again reverse to make as original structure
+        while(cur!=null){
+            fwd = cur.next;
+            cur.next = back;
+            back = cur;
+            cur = fwd;
+        }
+        return back;
+    }
+}
